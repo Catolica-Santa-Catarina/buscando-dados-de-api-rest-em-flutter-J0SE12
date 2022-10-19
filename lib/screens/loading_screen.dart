0f1bf94e17a1d05bbea16d812e50af6704b5a62a
Jose Vegini <josevegini12@gmail.com>
2022-10-19 20:58:33 -0300

    aplicativo geo-localização

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,17 +9,58 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> checkLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // serviço de localização desabilitado. Não será possível continuar
+      return Future.error('O serviço de localização está desabilitado.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Sem permissão para acessar a localização
+        return Future.error('Sem permissão para acesso à localização');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // permissões negadas para sempre
+      return Future.error('A permissão para acesso a localização foi negada para sempre. Não é possível pedir permissão.');
+    }
+  }
+
+  Future<void> getLocation() async{
+    await checkLocationPermission();
+
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+}
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      void initState() {
+        // é disparado quando o Widget for criado
+      }
+
+      Widget? build(BuildContext context) {
+        // disparado quando o objeto for construído e o widget aparecerá na tela
+        return null;
+      }
+
+      void deactivate() {
+        // é disparado quando o widget foi destruído
+      }
+    });
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // obtém a localização atual
-          },
-          child: const Text('Obter Localização'),
-        ),
-      ),
+
     );
   }
 }
